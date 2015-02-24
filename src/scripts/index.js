01c8648bd5d295bf53e533592a868a2f6899d0b5
davidsmith2 <davidsmith2@gmail.com>
2015-02-23 23:55:56 -0500

    stand up Marionette app

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,36 @@
-(function ($, _, Backbone) {
-})(jQuery, _, Backbone);
+(function ($, _, Backbone, Marionette) {
+
+    var App = new Marionette.Application();
+
+    var ContainerView = Marionette.ItemView.extend({
+        template: false,
+        el: '#container',
+        events: {
+            'keydown': 'onKeydown'
+        },
+        initialize: function () {
+            this.$el.attr('tabindex', -1).focus();
+        },
+        onKeydown: function (e) {
+            if (e.keyCode === 39) {
+                this.trigger('monkey:move');
+            }
+        }
+    });
+
+    var MonkeyView = Marionette.ItemView.extend({
+        template: false,
+        el: '#monkey',
+        move: function () {
+            var left = parseInt(this.$el.css('left'), 10) + 96;
+            this.$el.css('left', left + 'px');
+        }
+    });
+
+    var containerView = new ContainerView();
+
+    var monkeyView = new MonkeyView();
+
+    monkeyView.listenTo(containerView, 'monkey:move', monkeyView.move);
+
+})(jQuery, _, Backbone, Marionette);
